05711b64720e1b06820a79cd61ffc2c7206c1d72
Suraj Vishwakarma <smartcraze354@gmail.com>
2025-05-26 18:19:16 +0000

    keccaealgo

--- 01_hashing/index.js
+++ 01_hashing/index.js
@@ -1,8 +1,23 @@
 const crypto = require("crypto");
+const ether = require("ether");
 
 function sha256(text) {
     return crypto.createHash("sha256").update(text).digest("hex");
 }
 
-console.log("SHA-256 Hash:", sha256("my_secure_password"));
+// Example usage
+function authenticateUser(inputPassword, storedHash) {
+    const inputHash = sha256(inputPassword);
+    return inputHash === storedHash;
+}
 
+const storedHash = sha256("my_secure_password");
+console.log("SHA-256 Hash:", storedHash);
+
+
+// keccake algorithm
+
+
+function keccak256(text) {
+    return ether.keccak256(text);
+}
